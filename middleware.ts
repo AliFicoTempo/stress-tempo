7c9a1fa9fac05165c8552b5530134057ccbd7eb5
AliFicoTempo <ali.setraprabaperkasa@gmail.com>
2025-12-11 16:57:39 +0700

    update middleware users

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const publicPaths = ['/login', '/api/auth/login', '/api/drivers']
+const publicPaths = ['/login', '/api/auth/login', '/api/users/drivers']
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
